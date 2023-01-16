4b395e7c62b1a4a1fb31da20c92afc2d55553a11
Marcel Leyva <leyva.marccel@gmail.com>
2023-01-17 00:41:21 +0100

    Deploy

--- app/Components/Button.jsx
+++ app/Components/Button.jsx
@@ -1,7 +1,9 @@
+
 import styles from './button.module.css'
+
 
 export default function Button({text}) {
   return (
-    <button className={styles.button}>{text}</button>
+    <button  className={styles.button}>{text}</button>
   )
 }
--- app/Components/ButtonLogin.jsx
+++ app/Components/ButtonLogin.jsx
@@ -1,7 +1,60 @@
+'use client'
 import styles from './buttonLogin.module.css'
+import { getAuth } from 'firebase/auth';
+import {GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import { auth } from './firebase';
+import { useState } from 'react';
 
 export default function ButtonLogin({text, image, color}) {
+
+const [login, setLogin] = useState(false)
+const [state, setState] = useState('')
+
+const Login = async () => {
+  setLogin(!login)
+  
+  if(login === false) {
+   SignIn()
+  setState('El usuario se a logiado correctamente')
+  }else if(login === true) {
+    setState('')
+  }
+}
+
+const SignOut = () => {
+  auth.signOut()
+}
+
+const SignIn = () => {
+
+  const provider = new GoogleAuthProvider();
+
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    console.log(result)
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });  
+}
+
   return (
-    <button  style={{backgroundColor: color}} className={styles.button}><img height={20} width={20} src={image} alt="" /></button>
+    <>
+          <p>{state}</p>
+    <button onClick={Login} style={{backgroundColor: color}} className={styles.button}><img height={20} width={20} src={image} alt="" /></button>
+    </>
+
   )
 }
